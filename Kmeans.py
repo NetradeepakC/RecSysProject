836ceb9b9dfe48a377893e9bf7c780082eefcf8e
satvik-vm <satvik.verma@iiitb.ac.in>
2024-02-20 19:32:51 +0530

    wokred on kmeans and svd

--- Kmeans.py
+++ Kmeans.py
@@ -6,175 +6,154 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
-import random
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import random
-import math
-import scipy
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler # vector based
 
-
-def eucledian_distance(x1, x2):
-    return np.sqrt(np.sum((x1-x2)**2))  # vector based
-
-
-def minkowski_distance(x1, x2, p):
+def minkowski_dis(x_1, x_2, p):
     from scipy.spatial import distance
-    return distance.minkowski(x1, x2, p)
+    return distance.minkowski(x_1, x_2, p)
 
 
 class Kmeans:
-    # K = number of clusters
-    # iter = number of iterations
-    def __init__(self, K, iter, p, random_state=42):
+    def __init__(self, K, itterations, p, random_state=15):
         self.K = K
-        self.iter = iter
+        self.itterations = itterations
         self.p = p
-        # Initialising empty K clusters -> storing indicies
-        self.clusters = [[] for _ in range(self.K)]
-        # storing centroids
-        self.centroids = []
+        # self.clusters = [[] for _ in range(self.K)]
+        self.clusters = []
+        for _ in range(self.K):
+            self.clusters.append([])
+        self.centers = []
         self.inertia = 0.0
         self.random_state = random_state
         np.random.seed(random_state)
 
-    def plus_plus(self, ds: np.array, k, random_state=1000):
-        """
-        Create cluster centroids using the k-means++ algorithm.
-        Parameters
-        ----------
-        ds : numpy array
-            The dataset to be used for centroid initialization.
-        k : int
-            The desired number of clusters for which centroids are required.
-        Returns
-        -------
-        centroids : numpy array
-            Collection of k centroids as a numpy array.
-        Inspiration from here: https://stackoverflow.com/questions/5466323/how-could-one-implement-the-k-means-algorithm
-        """
+    # def k_plus_plus(self, datapoints: np.array, k, random_state=94):
+    #     np.random.seed(random_state)
+    #     centers = [datapoints[0]]
 
-        np.random.seed(random_state)
-        centroids = [ds[0]]
+    #     for _ in range(1, k):
+    #         random = np.random.rand()
+    #         distance_squared = np.array([min([np.inner(c-x, c-x) for c in centers]) for x in datapoints])
+    #         probabilities = distance_squared / distance_squared.sum()
+    #         cumulative_probabilities = probabilities.cumsum()
 
-        for _ in range(1, k):
-            dist_sq = np.array([min([np.inner(c-x, c-x)
-                               for c in centroids]) for x in ds])
-            probs = dist_sq/dist_sq.sum()
-            cumulative_probs = probs.cumsum()
-            r = np.random.rand()
+    #         for i, j in enumerate(cumulative_probabilities):
+    #             if random < j:
+    #                 center = i
+    #                 break
 
-            for j, p in enumerate(cumulative_probs):
-                if r < p:
-                    i = j
-                    break
+    #         centers.append(datapoints[center])
 
-            centroids.append(ds[i])
+    #     return np.array(centers)
 
-        return np.array(centroids)
-
-    def predict(self, X, choice=0):  # no fit required for unsupervised learning models
+    def predict(self, X, flag=0):
         self.X = X
-        self.n_samples, self.n_features = X.shape  # numpy N-d array
+        self.num_samples, self.num_features = self.X.shape
 
-        # initialise centroids
-        if choice == 0:
+        if flag == 0:
             np.random.seed(self.random_state)
-            # row , column full with zero
-            centroids = np.zeros((self.K, self.n_features))
-            for k in range(self.K):  # iterations of
-                # random centroids
-                centroid = X[np.random.choice(range(self.n_samples))]
-                centroids[k] = centroid
-            self.centroids = centroids
-        elif choice == 1:
-            self.centroids = self.plus_plus(X, self.K)
+            centers = np.zeros((self.K, self.num_features))
+            for k in range(self.K):
+                centroid = X[np.random.choice(range(self.num_samples))]
+                centers[k] = centroid
+            self.centers = centers
 
-        elif choice == 2:
-            self.centroids = self.naive_sharding(X, self.K)
+        elif flag == 1:
+            # self.centers = self.k_plus_plus(X, self.K)
+            lst_centers = [X[0]]
+            for _ in range(1, self.K):
+                random = np.random.rand()
+                distance_squared = np.array([min([np.inner(c-x, c-x) for c in lst_centers]) for x in X])
+                probabilities = distance_squared / distance_squared.sum()
+                cumulative_probabilities = probabilities.cumsum()
+                for i, j in enumerate(cumulative_probabilities):
+                    if random < j:
+                        center = i
+                        break
 
-        # optimise
-        for _ in range(self.iter):
-            # update clusters
-            self.clusters = self._create_clusters(self.centroids)
-            # update centroids
-            centroids_old = self.centroids  # for convergence test
-            self.centroids = self._get_centroids(self.clusters)
-            # print(self.centroids)
-            # get cetnroids assign mean value of cluster to the centroid
-            # check for convergence
-            if self._isConverged(centroids_old, self.centroids):
+                lst_centers.append(X[center])
+            self.centers = np.array(lst_centers)
+
+        # elif flag == 2:
+        #     self.centers = self.naive_sharding(X, self.K)
+
+        for _ in range(self.itterations):
+            # self.clusters = self.cluster_creation(self.centers)
+            self.clusters = [[] for _ in range(self.K)]
+            for idx, sample in enumerate(self.X):
+                # centroid_idx = self.nearby_centroid(sample, self.centers)
+                distances = [minkowski_dis(sample, point, self.p) for point in centers]
+                centroid_idx = np.argmin(distances)
+                self.clusters[centroid_idx].append(idx)
+            centroids_old = self.centers
+            # self.centers = self.get_centroids(self.clusters)
+            self.centers = np.zeros((self.K, self.num_features))
+            for cluster_idx, cluster in enumerate(self.clusters):
+                if (len(cluster) != 0):
+                    cluster_mean = np.mean(self.X[cluster], axis=0)
+                    self.centers[cluster_idx] = cluster_mean
+            # if self.convergence_test(centroids_old, self.centers):
+            distances = [minkowski_dis(centroids_old[i], self.centers[i], self.p) for i in range(self.K)]
+            if sum(distances) == 0:
                 break
-
-            # break
-
-        # classify the samples based on index of cluster
-        labels = self.getClusterLabels(self.clusters)
-        self.calculateInertia(X, labels)
-        return labels, self.centroids
-        # return cluster labels
-
-    def _create_clusters(self, centroids):
-        clusters = [[] for _ in range(self.K)]
-        for idx, sample in enumerate(self.X):
-            # find the closest centroid to classify, then put it in the specific cluster
-            # here cluster is an list of list
-            centroid_idx = self._closest_centroid(sample, centroids)
-            clusters[centroid_idx].append(idx)
-        return clusters
-
-    def function(self, point):
-        return minkowski_distance(self.sample, point, self.p)
-
-    def _closest_centroid(self, sample, centroids):
-        # distance metric
-        distances = [minkowski_distance(sample, point, self.p)
-                     for point in centroids]
-        # argmin for required parameters
-        closest_idx = np.argmin(distances)
-        # self.sample=sample
-        # closest_idx = scipy.optimize.minimize (fun=function ,x0=np.asarray(centroids))
-        return closest_idx
-
-    def _get_centroids(self, clusters):
-        # Dimension = N as N = number of features
-        centroids = np.zeros((self.K, self.n_features))
-        for cluster_idx, cluster in enumerate(clusters):
-            # calling mean on a specific current cluster as X is array of various clusters
-            if (len(cluster) != 0):
-                cluster_mean = np.mean(self.X[cluster], axis=0)
-                # set mean as newer centroids
-                centroids[cluster_idx] = cluster_mean
-        return centroids
-
-    def _isConverged(self, centroids_old, centroids):
-        distances = [minkowski_distance(
-            centroids_old[i], centroids[i], self.p) for i in range(self.K)]
-        # no more change occured in 2 iteration so converges
-        return sum(distances) == 0
-
-    def getClusterLabels(self, clusters):
-        labels = np.empty(self.n_samples)
-        for cluster_idx, cluster in enumerate(clusters):
+        # labels = self.cluster_labels(self.clusters)
+        labels = np.empty(self.num_samples)
+        for cluster_idx, cluster in enumerate(self.clusters):
             for sample_idx in cluster:
                 labels[sample_idx] = cluster_idx
-        return labels
-
-    def calculateInertia(self, datapoints: list[int], labels: list[any]) -> None:
+        # self.calculateInertia(X, labels)
         labels = labels.astype(int)
-        for idx, pts in enumerate(datapoints):
-            self.inertia += minkowski_distance(
-                self.centroids[labels[idx]], pts, 2)**2
+        for idx, pts in enumerate(X):
+            self.inertia += minkowski_dis(
+                self.centers[labels[idx]], pts, 2)**2
+        return labels, self.centers
+
+    # def cluster_creation(self, centers):
+    #     clusters = [[] for _ in range(self.K)]
+    #     for idx, sample in enumerate(self.X):
+    #         centroid_idx = self.nearby_centroid(sample, centers)
+    #         clusters[centroid_idx].append(idx)
+    #     return clusters
+
+    # def nearby_centroid(self, sample, centers):
+    #     # distance metric
+    #     distances = [minkowski_dis(sample, point, self.p) for point in centers]
+    #     closest_idx = np.argmin(distances)
+    #     return closest_idx
+
+    # def get_centroids(self, clusters):
+    #     centers = np.zeros((self.K, self.num_features))
+    #     for cluster_idx, cluster in enumerate(clusters):
+    #         if (len(cluster) != 0):
+    #             cluster_mean = np.mean(self.X[cluster], axis=0)
+    #             centers[cluster_idx] = cluster_mean
+    #     return centers
+
+    # def convergence_test(self, centroids_old, centers):
+    #     distances = [minkowski_dis(centroids_old[i], centers[i], self.p) for i in range(self.K)]
+    #     return sum(distances) == 0
+
+    # def cluster_labels(self, clusters):
+    #     labels = np.empty(self.num_samples)
+    #     for cluster_idx, cluster in enumerate(clusters):
+    #         for sample_idx in cluster:
+    #             labels[sample_idx] = cluster_idx
+    #     return labels
+
+    # def calculateInertia(self, datapoints: list[int], labels: list[any]) -> None:
+    #     labels = labels.astype(int)
+    #     for idx, pts in enumerate(datapoints):
+    #         self.inertia += minkowski_dis(
+    #             self.centers[labels[idx]], pts, 2)**2
 
     def predictPoint(self, datapoint):
         distances = []
-        for idx, i in enumerate(self.centroids):
+        for idx, i in enumerate(self.centers):
             if sum(i) == 0:
                 continue
             else:
-                distances.append(
-                    [minkowski_distance(datapoint, i, self.p), idx])
+                distances.append([minkowski_dis(datapoint, i, self.p), idx])
         distances.sort()
         return distances[0][1]
